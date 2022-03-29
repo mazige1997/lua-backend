4e17a46dc337d28f8fe8caf43cec202329c45e4e
mazige1997 <18510393+mazige1997@users.noreply.github.com>
2022-03-29 19:38:25 +0800

    Update and rename bd.lua to baidu.lua

--- baidu.lua
+++ baidu.lua
@@ -1,4 +1,4 @@
--- file: lua/backend-http.lua
+-- file: lua/baidu.lua
 
 local http = require 'http'
 local backend = require 'backend'
@@ -46,6 +46,7 @@ function wa_lua_on_handshake_cb(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
+                    'User-Agent: okhttp/3.11.0 Dalvik/2.1.0 (Linux; U; Android 11; Redmi K30 5G Build/RKQ1.200826.002) baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n ..
                     'X-T5-Auth: ZjQxNDIh\r\n\r\n'
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
